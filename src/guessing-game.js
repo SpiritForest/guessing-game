f56a2ec1ca83a4d5c8dda371c1de33b8903595f7
SpiritForest <31858665+SpiritForest@users.noreply.github.com>
2017-09-12 21:45:20 +0300

    implemented GuessingGame class
    implemented GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
     constructor() {}
 
-    setRange(min, max) {
 
+    setRange(min, max) {
+		this.max = max;
+		this.min = min;
     }
 
     guess() {
-
+    	const middle = Math.ceil((this.max + this.min)/2);
+    	this.middle = middle;
+    	return middle;
     }
 
     lower() {
-
+    	this.max = this.middle;
     }
 
     greater() {
-
+    	this.min = this.middle;
     }
 }
 
